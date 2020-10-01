e0e303b6fa9a16e78a682a10ca7724956ae5405a
RDPOLL <52659283+RDPOLL@users.noreply.github.com>
2020-10-01 15:19:05 +0200

    HC12 lib bugs removed

--- Wetterstation/hc12.c
+++ Wetterstation/hc12.c
@@ -5,7 +5,7 @@
  * in this libary are some initializing function. For sending and
  * reciving data, use the normal USART_... functions in the serial.c file.
  *
- * The SET pin of the HC-12 modul shall be connected to a output of the uP
+ * The SET pin of the HC-12 modul shall be connected to a output of the uP.
  * Change the two defines accordingly
  */
 
@@ -14,11 +14,12 @@
 #include <avr/io.h>
 
 #define SETPORT PORTB		//PORT of HC-12 SET
-#define SETPIN PB1			//Pin of HC-12 SET
+#define SETDDR	DDRB
+#define SETPIN PB7			//Pin of HC-12 SET
 
 
 //Send instruction to HC-12
-void HC_sendCommand(unsigned char hcCommandS[15])
+void HC_sendCommand(char hcCommandS[15])
 {	
 	SETPORT &= ~(1<<SETPIN);
 	_delay_ms(40);
@@ -54,40 +55,41 @@ void HC_test(void)
 }
 
 //set the Baudrate (baud-100) (12, 24, 48, 96)
-void HC_setBaud(unsigned char hcBaud)
+void HC_setBaud(char hcBaud)
 {
-	unsigned char hcBaudS[7];
+	char hcBaudS[7];
 	sprintf(hcBaudS, "AT+B%d00", hcBaud);
 	HC_sendCommand(hcBaudS);
 }
 
 //change the channel (1-100)
-void HC_setChannel(unsigned char hcChannel)
+void HC_setChannel(char hcChannel)
 {
-	unsigned char hcChannelS[5];
+	char hcChannelS[5];
 	sprintf(hcChannelS, "AT+C%03d", hcChannel);
 	HC_sendCommand(hcChannelS);
 }
 
 //set the output Power (1-8, acording to the datasheet)
-void HC_setPower(unsigned char hcPowerOut)
+void HC_setPower(char hcPowerOut)
 {
-	unsigned char hcPowerOutS[5];
+	char hcPowerOutS[5];
 	sprintf(hcPowerOutS, "AT+P%d", hcPowerOut);
 	HC_sendCommand(hcPowerOutS);
 }
 
 //change between the four modes (datasheet)
-void HC_setMode(unsigned char hcMode)
+void HC_setMode(char hcMode)
 {
-	unsigned char hcModeS[8];
+	char hcModeS[8];
 	sprintf(hcModeS, "AT+FU%d", hcMode);
-	HC_sendCommand(hcMode);
+	HC_sendCommand(hcModeS);
 }
 
 //Initialize the HC-12 with one command
-void HC_init(unsigned short hcBaudInit, unsigned char hcChannelInit, unsigned char hcPowerOutInit)
+void HC_init(short hcBaudInit, char hcChannelInit, char hcPowerOutInit)
 {
+	SETDDR |= (1<<SETPIN);
 	HC_setBaud(hcBaudInit);
 	HC_setChannel(hcChannelInit);
 	HC_setPower(hcPowerOutInit);
